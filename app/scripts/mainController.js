fafedd396132bb35dbe10a4c6b9e2ac5a4c26e27
Nuno Faria <nuno@MBP-de-Nuno.lan>
2015-07-06 22:56:13 +0100

    start node server migration
    start to migrate flapper to a node server

--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -1,41 +1,34 @@
-'use strict';
 /*global angular,alert*/
 var mainController = angular.module('mainController', []);
-
-mainController.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
-
-    $scope.posts = posts.posts;
-
-    $scope.addPost = function() {
-        if (!$scope.title || $scope.title === '') {
-            alert('A title is needed!');
-            return;
-        }
-        $scope.posts.push({
-            title: $scope.title,
-            link: $scope.link,
-            votes: 0,
-            comments: []
-        });
-        $scope.title = '';
-        $scope.link = '';
-    };
-
-    $scope.upVote = function(post) {
-        post.votes++;
-    };
-
-    $scope.downVote = function(post) {
-        if (post.votes > 0) {
-            post.votes--;
-        }
-    };
+mainController.controller('MainCtrl', ['$scope', 'posts', function ($scope, posts) {
+  $scope.posts = posts.posts;
+  $scope.addPost = function () {
+    if (!$scope.title || $scope.title === '') {
+      alert('A title is needed!');
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      votes: 0,
+      comments: []
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+  $scope.upVote = function (post) {
+    post.votes++;
+  };
+  $scope.downVote = function (post) {
+    if (post.votes > 0) {
+      post.votes--;
+    }
+  };
 }]);
-
 // create posts service
-mainController.factory('posts', [function() {
-    var o = {
-        posts: []
-    };
-    return o;
+mainController.factory('posts', [function () {
+  var o = {
+    posts: []
+  };
+  return o;
 }]);
